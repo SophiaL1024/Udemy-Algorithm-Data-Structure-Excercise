7d6d2d1c0963f524d1b31e6bed81eb64cb79002a
SophiaL1024 <sophiafengliu@gmail.com>
2021-08-05 23:14:11 -0700

    refactor anagram validation

--- frequencyCounterValidAnagram.js
+++ frequencyCounterValidAnagram.js
@@ -7,7 +7,9 @@
 // validAnagram('awesome', 'awesom') // false
 // validAnagram('qwerty', 'qeywrt') // true
 // validAnagram('texttwisttime', 'timetwisttext') // true
-const validAnagram = function(str1,str2) {
+
+//solution1: define two objects of letter frequency and compare the two objects
+const validAnagram1 = function(str1,str2) {
   //if two strings has different length, return false
   if (str1.length !== str2.length) {
     return false;
@@ -35,5 +37,32 @@ const validAnagram = function(str1,str2) {
   return true;
 };
 
-console.log(validAnagram('texttwisttime', 'timetwisttext'));
-console.log(validAnagram('awesome', 'awesom'));
+
+//solution2: compare letter frequency in the second string loop
+const validAnagram2 = function(str1,str2) {
+  //if two strings has different length, return false
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  //collect letter frequencies of first string by object
+  const obj1 = {};
+
+  for (let letter of str1) {
+    obj1[letter] = (obj1[letter] || 0) + 1;
+  }
+
+  //loop the 2nd string, check if letter of str2 has the same frequency as str1
+  for (let letter of str2) {
+    if (!obj1[letter]) {
+      return false;
+    } else {
+      obj1[letter]--;
+    }
+  }
+  return true;
+};
+
+console.log(validAnagram1('texttwisttime', 'timetwisttext'));
+console.log(validAnagram1('awesome', 'awesom'));
+console.log(validAnagram2('texttwisttime', 'timetwisttext'));
+console.log(validAnagram2('awesome', 'awesom'));
