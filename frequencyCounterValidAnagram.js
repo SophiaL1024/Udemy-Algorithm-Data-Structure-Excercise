de26134095b687335f0ab5bbac9ee15c558d9c24
SophiaL1024 <sophiafengliu@gmail.com>
2021-08-05 22:03:16 -0700

    pattern  of frequency counter

--- frequencyCounterValidAnagram.js
+++ frequencyCounterValidAnagram.js
@@ -7,6 +7,33 @@
 // validAnagram('awesome', 'awesom') // false
 // validAnagram('qwerty', 'qeywrt') // true
 // validAnagram('texttwisttime', 'timetwisttext') // true
-function validAnagram() {
- 
-}
+const validAnagram = function(str1,str2) {
+  //if two strings has different length, return false
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  //collect letter frequencies for each string by object
+  //define two objects
+  const obj1 = {};
+  const obj2 = {};
+  for (let letter of str1) {
+    obj1[letter] = (obj1[letter] || 0) + 1;
+  }
+  for (let letter of str2) {
+    obj2[letter] = (obj2[letter] || 0) + 1;
+  }
+  //if the two objects have diffrent length, return false
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  //if the same letter has diffrent frequency in the two objects, return false
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(validAnagram('texttwisttime', 'timetwisttext'));
+console.log(validAnagram('awesome', 'awesom'));
