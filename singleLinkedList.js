f3d3d494abae6321338bbe74767a2df6f461d3fd
SophiaL1024 <sophiafengliu@gmail.com>
2021-08-14 22:04:36 -0700

    linked list reverse method

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -120,8 +120,18 @@ class SingleLinkedList {
     }
   }
   reverse() {
-    this.tail = temp;
-  
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let prev = null;
+    let next;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
   }
 }
 
