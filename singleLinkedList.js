104e465d358d35c20c80eca6acbeb1080276e7ea
SophiaL1024 <sophiafengliu@gmail.com>
2021-08-14 17:04:12 -0700

    linked list pop method

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -22,6 +22,32 @@ class SingleLinkedList {
     this.length++;
 
   }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      current = current.next;
+    }
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+    let current = this.head;
+    let newTail = current;
+    // if current is the last node, get out of the loop, then the newTail is the one before the last node.
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 const list = new SingleLinkedList;
@@ -30,4 +56,6 @@ list.push(2);
 list.push(3);
 list.push(4);
 list.push(5);
+console.log(list);
+console.log(list.pop());
 console.log(list);
