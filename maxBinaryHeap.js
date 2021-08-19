074f69ed0ac82577c5e3c8c37ea24a95396c6589
SophiaL1024 <sophiafengliu@gmail.com>
2021-08-18 21:19:20 -0700

    maxBinaryHeap extraMax method

--- maxBinaryHeap.js
+++ maxBinaryHeap.js
@@ -15,6 +15,32 @@ class MaxBinaryHeap {
     }
     return this.value
   }
+  extractMax(){
+    const end=this.value.pop();
+    this.value[0]=end;
+    let index=0;
+    //bubble down
+    let leftChild=2*index+1;
+    let rightChild=2*index+2;
+    while(this.value[index]<this.value[leftChild]||this.value[index]<this.value[rightChild]){
+      if(this.value[leftChild]<this.value[rightChild]){
+        const temp=this.value[index];
+        this.value[index]=this.value[rightChild];
+        this.value[rightChild]=temp;
+        index=rightChild;
+        leftChild=2*index+1;
+        rightChild=2*index+2;
+      }else {
+        const temp=this.value[index];
+        this.value[index]=this.value[leftChild];
+        this.value[leftChild]=temp;
+        index=leftChild;
+        leftChild=2*index+1;
+        rightChild=2*index+2;
+      }
+    }
+    return this.value;
+  }
 }
 const heap=new MaxBinaryHeap();
 heap.insert(41)
@@ -24,5 +50,6 @@ heap.insert(18)
 heap.insert(27)
 heap.insert(12)
 heap.insert(55)
-heap.insert(45)
+console.log(heap)
+heap.extractMax();
 console.log(heap)
